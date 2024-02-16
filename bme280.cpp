358ec8ca95458f44c176dee0a8e390feecec442a
Maksym Mamontov <madf@madf.info>
2024-02-16 22:20:32 +0200

    Add BME interface.

---
+++ bme280.cpp
@@ -0,0 +1,235 @@
+#include "bme280.h"
+
+namespace
+{
+
+enum Registers : uint8_t {
+    DIG_T1 = 0x88,
+    DIG_T2 = 0x8A,
+    DIG_T3 = 0x8C,
+
+    DIG_P1 = 0x8E,
+    DIG_P2 = 0x90,
+    DIG_P3 = 0x92,
+    DIG_P4 = 0x94,
+    DIG_P5 = 0x96,
+    DIG_P6 = 0x98,
+    DIG_P7 = 0x9A,
+    DIG_P8 = 0x9C,
+    DIG_P9 = 0x9E,
+
+    DIG_H1 = 0xA1,
+    DIG_H2 = 0xE1,
+    DIG_H3 = 0xE3,
+    DIG_H4 = 0xE4,
+    DIG_H5 = 0xE5,
+    DIG_H6 = 0xE7,
+
+    CHIPID = 0xD0,
+    VERSION = 0xD1,
+    SOFTRESET = 0xE0,
+
+    CAL26 = 0xE1, // R calibration stored in 0xE1-0xF0
+
+    CONTROLHUMID = 0xF2,
+    STATUS = 0XF3,
+    CONTROL = 0xF4,
+    CONFIG = 0xF5,
+    PRESSUREDATA = 0xF7,
+    TEMPDATA = 0xFA,
+    HUMIDDATA = 0xFD
+};
+
+template <typename T>
+T fromThree(T h, T l, T xl)
+{
+    return (h << 12) | (l << 4) | (xl >> 4);
+}
+
+uint32_t fromTwo(uint32_t h, uint32_t l)
+{
+    return (h << 8) | l;
+}
+
+}
+
+BME280::BME280(I2C_HandleTypeDef& handle, uint8_t address) noexcept
+    : m_dev(handle, address)
+{
+}
+
+auto BME280::init(Mode mode,
+                        Sampling st,
+                  Sampling sp,
+                  Sampling sh,
+                  Filter filter,
+                  Standby dur) noexcept -> Status
+{
+    if (!m_dev.ready())
+        return Status::NOT_READY;
+    uint8_t id = 0;
+    if (!readId(id))
+        return Status::READING_ID_FAILURE;
+    //if (id != 0x60)
+    //    return Status::BAD_ID;
+
+    if (!m_dev.writeReg(Registers::SOFTRESET, 0xB6))
+        return Status::SOFT_RESET_FAILURE;
+
+    for (bool res = true; res;)
+    {
+        HAL_Delay(10);
+        if (!isReadingCalibration(res))
+            return Status::READING_CALIBRATION_FAILURE;
+    }
+
+    if (!readCoefficients())
+        return Status::READING_COEFFICIENTS_FAILURE;
+
+    if (!setSampling(mode, st, sp, sh, filter, dur))
+        return Status::SETTING_SAMPLING_FAILURE;
+    HAL_Delay(100);
+
+    return Status::OK;
+}
+
+auto BME280::init() noexcept -> Status
+{
+    return init(Mode::NORMAL,
+                 Sampling::X16,
+                Sampling::X16,
+                Sampling::X16,
+                Filter::OFF,
+                Standby::MS_0_5);
+}
+
+bool BME280::isReadingCalibration(bool& res)
+{
+    uint8_t status = 0;
+    if (!readStatus(status))
+        return false;
+    res = (status & 0x01) != 0;
+    return true;
+}
+
+bool BME280::readCoefficients()
+{
+    Calibration calib;
+    uint8_t h4 = 0;
+    uint16_t h5 = 0;
+    bool res =  read(Registers::DIG_T1, calib.digT1)
+             && read(Registers::DIG_T2, calib.digT2)
+             && read(Registers::DIG_T3, calib.digT3)
+
+             && read(Registers::DIG_P1, calib.digP1)
+             && read(Registers::DIG_P2, calib.digP2)
+             && read(Registers::DIG_P3, calib.digP3)
+             && read(Registers::DIG_P4, calib.digP4)
+             && read(Registers::DIG_P5, calib.digP5)
+             && read(Registers::DIG_P6, calib.digP6)
+             && read(Registers::DIG_P7, calib.digP7)
+             && read(Registers::DIG_P8, calib.digP8)
+             && read(Registers::DIG_P9, calib.digP9)
+
+             && read(Registers::DIG_H1, calib.digH1)
+             && read(Registers::DIG_H2, calib.digH2)
+             && read(Registers::DIG_H3, calib.digH3)
+             && read(Registers::DIG_H4, h4)
+             && read(Registers::DIG_H5, h5)
+             && read(Registers::DIG_H6, calib.digH6);
+    if (!res)
+        return false;
+
+    calib.digH4 = (h4 << 4) + (h5 & 0x000F);
+    calib.digH5 = (h5 >> 4);
+
+    calib.tFine = 0;
+
+    m_calib = calib;
+    return true;
+}
+
+bool BME280::setSampling(Mode mode, Sampling st, Sampling sp, Sampling sh, Filter filter, Standby dur)
+{
+    return m_dev.writeReg(Registers::CONTROL,      static_cast<uint8_t>(Mode::SLEEP))
+        && m_dev.writeReg(Registers::CONTROLHUMID, static_cast<uint8_t>(sh))
+        && m_dev.writeReg(Registers::CONFIG,       (static_cast<uint8_t>(dur) << 5) + (static_cast<uint8_t>(filter) << 2))
+        && m_dev.writeReg(Registers::CONTROL,      (static_cast<uint8_t>(st) << 5)  + (static_cast<uint8_t>(sp) << 2) + static_cast<uint8_t>(mode));
+}
+
+bool BME280::readRaw(void* data)
+{
+    return m_dev.readRegs(Registers::PRESSUREDATA, data, 8);
+}
+
+bool BME280::readId(uint8_t& res)
+{
+    return m_dev.readReg(Registers::CHIPID, res);
+}
+
+bool BME280::readStatus(uint8_t& res)
+{
+    return m_dev.readReg(Registers::STATUS, res);
+}
+
+bool BME280::readData(uint32_t& h, uint32_t& p, int32_t& t)
+{
+    uint8_t regs[8];
+    if (!m_dev.readRegs(Registers::PRESSUREDATA, regs, sizeof(regs)))
+        return false;
+    t = compT(fromThree<int32_t>(regs[3], regs[4], regs[5])); // This should go first
+    p = compP(fromThree<uint32_t>(regs[0], regs[1], regs[2]));
+    h = compH(fromTwo(regs[6], regs[7]));
+    return true;
+}
+
+int32_t BME280::compT(int32_t v)
+{
+    const int32_t var1 = ((((v / 8) - ((int32_t)m_calib.digT1 * 2))) * ((int32_t)m_calib.digT2)) >> 11;
+    const int32_t var2 = (((((v / 16) - ((int32_t)m_calib.digT1)) * ((v / 16) - ((int32_t)m_calib.digT1))) >> 12) * ((int32_t)m_calib.digT3)) >> 14;
+    m_calib.tFine = var1 + var2;
+    return (m_calib.tFine * 5 + 128) / 256;
+}
+
+uint32_t BME280::compP(uint32_t v)
+{
+    int64_t var1 = ((int64_t)m_calib.tFine) - 128000;
+    int64_t var2 = var1 * var1 * (int64_t)m_calib.digP6;
+    var2 = var2 + ((var1*(int64_t)m_calib.digP5)<<17);
+    var2 = var2 + (((int64_t)m_calib.digP4)<<35);
+    var1 = ((var1 * var1 * (int64_t)m_calib.digP3)>>8) + ((var1 * (int64_t)m_calib.digP2)<<12);
+    var1 = (((((int64_t)1)<<47)+var1))*((int64_t)m_calib.digP1)>>33;
+    if(var1 == 0)
+        return 0; // avoid exception caused by division by zero
+    int64_t p = 1048576 - v;
+    p = (((p<<31) - var2)*3125)/var1;
+    var1 = (((int64_t)m_calib.digP9) * (p>>13) * (p>>13)) >> 25;
+    var2 =(((int64_t)m_calib.digP8) * p) >> 19;
+    return ((p + var1 + var2) >> 8) + (((int64_t)m_calib.digP7)<<4);
+}
+
+uint32_t BME280::compH(uint32_t v)
+{
+    int32_t v_x1_u32r = (m_calib.tFine - ((int32_t)76800));
+    v_x1_u32r = (((((v << 14) - (((int32_t)m_calib.digH4) << 20) - (((int32_t)m_calib.digH5) * v_x1_u32r)) + ((int32_t)16384)) >> 15) * (((((((v_x1_u32r * ((int32_t)m_calib.digH6)) >> 10) * (((v_x1_u32r * ((int32_t)m_calib.digH3)) >> 11) + ((int32_t)32768))) >> 10) + ((int32_t)2097152)) * ((int32_t)m_calib.digH2) + 8192) >> 14));
+    v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) * ((int32_t)m_calib.digH1)) >> 4));
+    v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
+    v_x1_u32r = (v_x1_u32r > 419430400? 419430400: v_x1_u32r);
+    return (uint32_t)(v_x1_u32r>>12);
+}
+
+std::string toString(BME280::Status s) noexcept
+{
+    switch (s)
+    {
+        case BME280::Status::NOT_READY: return "Not ready";
+        case BME280::Status::READING_ID_FAILURE: return "Failed to read device id";
+        case BME280::Status::BAD_ID: return "Bad device id";
+        case BME280::Status::SOFT_RESET_FAILURE: return "Failed to perform soft reset";
+        case BME280::Status::READING_CALIBRATION_FAILURE: return "Failed to read calibration";
+        case BME280::Status::READING_COEFFICIENTS_FAILURE: return "Failed to read coefficients";
+        case BME280::Status::SETTING_SAMPLING_FAILURE: return "Failed to set sampling";
+        case BME280::Status::OK: return "Ok";
+        default: return "<unknown>";
+    }
+}
---
+++ bme280.h
@@ -0,0 +1,129 @@
+#pragma once
+
+#include "i2cdev.h"
+
+#include <string>
+#include <cstdint>
+
+class BME280
+{
+    public:
+        enum class Sampling : uint8_t
+        {
+            SKIP = 0b000,
+            X1   = 0b001,
+            X2   = 0b010,
+            X4   = 0b011,
+            X8   = 0b100,
+            X16  = 0b101
+        };
+
+        enum class Mode : uint8_t
+        {
+            SLEEP  = 0b00,
+            FORCED = 0b01,
+            NORMAL = 0b11
+        };
+
+        enum class Filter : uint8_t
+        {
+            OFF = 0b000,
+            X2  = 0b001,
+            X4  = 0b010,
+            X8  = 0b011,
+            X16 = 0b100
+        };
+
+        enum class Standby : uint8_t
+        {
+            MS_0_5  = 0b000,
+            MS_10   = 0b110,
+            MS_20   = 0b111,
+            MS_62_5 = 0b001,
+            MS_125  = 0b010,
+            MS_250  = 0b011,
+            MS_500  = 0b100,
+            MS_1000 = 0b101
+        };
+
+        enum class Status
+        {
+            OK,
+            NOT_READY,
+            READING_ID_FAILURE,
+            BAD_ID,
+            SOFT_RESET_FAILURE,
+            READING_CALIBRATION_FAILURE,
+            READING_COEFFICIENTS_FAILURE,
+            SETTING_SAMPLING_FAILURE
+        };
+
+        BME280(I2C_HandleTypeDef& handle, uint8_t address) noexcept;
+
+        Status init(Mode mode,
+                    Sampling st,
+                    Sampling sp,
+                    Sampling sh,
+                    Filter filter,
+                    Standby dur) noexcept;
+        Status init() noexcept;
+
+        bool ready() noexcept { return m_dev.ready(); }
+
+        bool readId(uint8_t& res) noexcept;
+        bool readStatus(uint8_t& res) noexcept;
+        bool readData(uint32_t& h, uint32_t& p, int32_t& t) noexcept;
+
+        bool readRaw(void* data) noexcept;
+
+    private:
+        I2C::Device m_dev;
+
+        struct Calibration
+        {
+            // Temperature compensation values
+            uint16_t digT1;
+            int16_t  digT2;
+            int16_t  digT3;
+
+            // Pressure compensation values
+            uint16_t digP1;
+            int16_t  digP2;
+            int16_t  digP3;
+            int16_t  digP4;
+            int16_t  digP5;
+            int16_t  digP6;
+            int16_t  digP7;
+            int16_t  digP8;
+            int16_t  digP9;
+
+            // Humidity compensation values
+            uint8_t  digH1;
+            int16_t  digH2;
+            uint8_t  digH3;
+            int16_t  digH4;
+            int16_t  digH5;
+            int8_t   digH6;
+
+            // Intermediate temperature coefficient
+            int32_t tFine;
+        };
+
+        Calibration m_calib;
+
+        bool isReadingCalibration(bool& res);
+        bool readCoefficients();
+        bool setSampling(Mode mode, Sampling st, Sampling sp, Sampling sh, Filter filter, Standby dur);
+
+        template <typename T>
+        bool read(uint8_t regNum, T& res)
+        {
+            return m_dev.readRegs(regNum, &res, sizeof(res));
+        }
+
+        int32_t compT(int32_t v);
+        uint32_t compP(uint32_t v);
+        uint32_t compH(uint32_t v);
+};
+
+std::string toString(BME280::Status s) noexcept;
