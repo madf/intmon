c47f0cc44220b825f58b88a1e3d75e43f11a05db
Maksym Mamontov <madf@madf.info>
2024-06-16 15:28:40 +0300

    Enchance button.

--- button.h
+++ button.h
@@ -6,23 +6,41 @@ namespace Buttons
 {
 
 template <typename Pin>
-struct Button
+class Button
 {
     static_assert(GPIO::isPin_v<Pin>, "LED must be driven by a GPIO pin");
 
-    static void init()
-    {
-        Pin::enable();
-        Pin::setMode(GPIO::Mode::INPUT);
-        Pin::setOutputType(GPIO::OutputType::PUSH_PULL);
-        Pin::setPull(GPIO::Pull::UP);
-        Pin::setSpeed(GPIO::Speed::LOW);
-    }
+    public:
+        enum class State { PRESSED, RELEASED, NO_CHANGE };
 
-    static bool isPressed()
-    {
-        return Pin::get();
-    }
+        Button()
+            : m_state(false)
+        {
+            Pin::enable();
+            Pin::setMode(GPIO::Mode::INPUT);
+            Pin::setOutputType(GPIO::OutputType::PUSH_PULL);
+            Pin::setPull(GPIO::Pull::UP);
+            Pin::setSpeed(GPIO::Speed::LOW);
+        }
+
+        State get()
+        {
+            const auto state = Pin::get();
+            if (state && !m_state)
+            {
+                m_state = true;
+                return State::PRESSED;
+            }
+            if (!state && m_state)
+            {
+                m_state = false;
+                return State::RELEASED;
+            }
+            return State::NO_CHANGE;
+        }
+
+    private:
+        bool m_state;
 };
 
 }
