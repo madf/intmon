3f0b568ccb111158ba38b5c17cce4db0c6d71d93
Maksym Mamontov <madf@madf.info>
2025-02-17 17:23:47 +0200

    Implement date and time editing.

--- menu.cpp
+++ menu.cpp
@@ -1,17 +1,63 @@
 #include "menu.h"
 
+#include "timer.h"
 #include "display.h"
 #include "fonts.h"
 #include "keyboard.h"
 #include "rtc.h"
+#include "datetime.h"
+#include "utils.h" // lz
 
 #include <chrono>
+#include <utility> // std::unreachable
+
+namespace
+{
+
+constexpr uint8_t daysInMonth(uint16_t year, uint8_t month)
+{
+    switch (month)
+    {
+        case 1:  return 31; // Jan
+        case 2:             // Feb
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+                return 29;
+            return 28;
+        case 3:  return 31; // Mar
+        case 4:  return 30; // Apr
+        case 5:  return 31; // May
+        case 6:  return 30; // Jun
+        case 7:  return 31; // Jul
+        case 8:  return 31; // Aug
+        case 9:  return 30; // Sep
+        case 10: return 31; // Oct
+        case 11: return 30; // Nov
+        case 12: return 31; // Dec
+    };
+    std::unreachable();
+}
+
+void incInRange(uint8_t& v, uint8_t l, uint8_t h)
+{
+    if (v == h)
+        v = l;
+    else
+        ++v;
+}
+
+void decInRange(uint8_t& v, uint8_t l, uint8_t h)
+{
+    if (v == l)
+        v = h;
+    else --v;
+}
+
+}
 
 Menu::Menu(Display& d, Fonts& f, Keyboard& k)
     : m_display(d),
       m_fonts(f),
-      m_keyboard(k),
-      m_blink(std::chrono::milliseconds(300))
+      m_keyboard(k)
 {
 }
 
@@ -72,17 +118,52 @@ void Menu::runEdit()
 void Menu::runEditDate()
 {
     auto date = RTC::Device::getDate();
-    while (true)
+    DatePart part = DatePart::Year;
+    bool showPart = true;
+    showEditDate(date, part, showPart);
+    Timer blink(std::chrono::milliseconds(300));
+    bool done = false;
+    const auto next = [](DatePart p){ return static_cast<DatePart>((std::to_underlying(p) + 1) % 3); };
+    const auto incPart = [](DatePart p, Date& d)
+    {
+        switch (p)
+        {
+            case DatePart::Year: ++d.year; break;
+            case DatePart::Month: incInRange(d.month, 1, 12); break;
+            case DatePart::Day: incInRange(d.day, 1, daysInMonth(d.year, d.month)); break;
+        };
+    };
+    const auto decPart = [](DatePart p, Date& d)
+    {
+        switch (p)
+        {
+            case DatePart::Year: --d.year; break;
+            case DatePart::Month: decInRange(d.month, 1, 12); break;
+            case DatePart::Day: decInRange(d.day, 1, daysInMonth(d.year, d.month)); break;
+        };
+    };
+    while (!done)
     {
         const auto e = m_keyboard.get();
         using Action = Keyboard::Action;
         switch (e.action.value())
         {
-            case Action::Enter: runEdit(); break;
-            case Action::Plus:  nextMenu(); show(); break;
-            case Action::Minus: prevMenu(); show(); break;
+            case Action::Enter:
+                if (part == DatePart::Day)
+                    done = true;
+                else
+                    part = next(part);
+                break;
+            case Action::Plus:  incPart(part, date); break;
+            case Action::Minus: decPart(part, date); break;
             case Action::Exit:  return;
         };
+        if (blink.expired())
+        {
+            showPart = !showPart;
+            blink.reset();
+        }
+        showEditDate(date, part, showPart);
     }
     RTC::Device::setDate(date);
 }
@@ -90,17 +171,97 @@ void Menu::runEditDate()
 void Menu::runEditTime()
 {
     auto time = RTC::Device::getTime();
-    while (true)
+    TimePart part = TimePart::Hour;
+    bool showPart = true;
+    showEditTime(time, part, showPart);
+    Timer blink(std::chrono::milliseconds(300));
+    bool done = false;
+    const auto next = [](TimePart p){ return static_cast<TimePart>((std::to_underlying(p) + 1) % 3); };
+    const auto incPart = [](TimePart p, Time& t)
+    {
+        switch (p)
+        {
+            case TimePart::Hour: incInRange(t.hour, 0, 23); break;
+            case TimePart::Minute: incInRange(t.minute, 0, 59); break;
+            case TimePart::Second: incInRange(t.second, 0, 59); break;
+        };
+    };
+    const auto decPart = [](TimePart p, Time& t)
+    {
+        switch (p)
+        {
+            case TimePart::Hour: decInRange(t.hour, 0, 23); break;
+            case TimePart::Minute: decInRange(t.minute, 0, 59); break;
+            case TimePart::Second: decInRange(t.second, 0, 59); break;
+        };
+    };
+    while (!done)
     {
         const auto e = m_keyboard.get();
         using Action = Keyboard::Action;
         switch (e.action.value())
         {
-            case Action::Enter: runEdit(); break;
-            case Action::Plus:  nextMenu(); show(); break;
-            case Action::Minus: prevMenu(); show(); break;
+            case Action::Enter:
+                if (part == TimePart::Second)
+                    done = true;
+                else
+                    part = next(part);
+                break;
+            case Action::Plus:  incPart(part, time); break;
+            case Action::Minus: decPart(part, time); break;
             case Action::Exit:  return;
         };
+        if (blink.expired())
+        {
+            showPart = !showPart;
+            blink.reset();
+        }
+        showEditTime(time, part, showPart);
     }
     RTC::Device::setTime(time);
+}
+
+void Menu::showEditDate(const Date& dt, DatePart part, bool showPart)
+{
+    m_display.clear();
+    if (!showPart)
+    {
+        std::string v;
+        if (part == DatePart::Year)
+            v += "    -";
+        else
+            v += std::to_string(dt.year) + "-";
+        if (part == DatePart::Month)
+            v += "  -";
+        else
+            v += lz(dt.month) + "-";
+        if (part != DatePart::Day)
+            v += lz(dt.day);
+        m_display.printAt(0, 0, m_fonts.big, v);
+    }
+    else
+        m_display.printAt(0, 0, m_fonts.big, toString(dt));
+    m_display.update();
+}
+
+void Menu::showEditTime(const Time& tm, TimePart part, bool showPart)
+{
+    m_display.clear();
+    if (!showPart)
+    {
+        std::string v;
+        if (part == TimePart::Hour)
+            v += "  :";
+        else
+            v += lz(tm.hour) + ":";
+        if (part == TimePart::Minute)
+            v += "  :";
+        else
+            v += lz(tm.minute) + ":";
+        if (part != TimePart::Second)
+            v += lz(tm.second);
+    }
+    else
+        m_display.printAt(0, 0, m_fonts.big, toString(tm));
+    m_display.update();
 }
--- menu.h
+++ menu.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include "timer.h"
-
 #include <cstdint>
 
 class Display;
 struct Fonts;
 class Keyboard;
+struct Date;
+struct Time;
 
 class Menu
 {
@@ -17,12 +17,11 @@ class Menu
     private:
         enum class Edit : uint8_t { Date = 0, Time = 1 };
         enum class DatePart : uint8_t { Year = 0, Month = 1, Day = 2 };
-        enum class TimePart : uint8_t { Hour = 0, Minute = 1 };
+        enum class TimePart : uint8_t { Hour = 0, Minute = 1, Second = 2 };
         Edit m_edit = Edit::Date;
         Display& m_display;
         Fonts& m_fonts;
         Keyboard& m_keyboard;
-        Timer m_blink;
 
         void show();
         void nextMenu();
@@ -30,4 +29,6 @@ class Menu
         void runEdit();
         void runEditDate();
         void runEditTime();
+        void showEditDate(const Date& dt, DatePart part, bool showPart);
+        void showEditTime(const Time& tm, TimePart part, bool showPart);
 };
